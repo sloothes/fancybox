f696aeef05d7f2a0322c63694d619b36db78cd41
sloothes <sloothes.com@gmail.com>
2019-08-08 05:14:26 +0300

    Update jquery.fancybox-1.3.4.js

--- fancybox/jquery.fancybox-1.3.4.js
+++ fancybox/jquery.fancybox-1.3.4.js
@@ -1049,14 +1049,14 @@
 			.append('<div class="fancybox-bg" id="fancybox-bg-n"></div><div class="fancybox-bg" id="fancybox-bg-ne"></div><div class="fancybox-bg" id="fancybox-bg-e"></div><div class="fancybox-bg" id="fancybox-bg-se"></div><div class="fancybox-bg" id="fancybox-bg-s"></div><div class="fancybox-bg" id="fancybox-bg-sw"></div><div class="fancybox-bg" id="fancybox-bg-w"></div><div class="fancybox-bg" id="fancybox-bg-nw"></div>')
 			.appendTo( wrap );
 
-        content = $('<div id="fancybox-content"></div>');
-        close = $('<a id="fancybox-close"></a>');
-        title = $('<div id="fancybox-title"></div>');
+		outer.append(
+            content = $('<div id="fancybox-content"></div>'),
+            close = $('<a id="fancybox-close"></a>'),
+            title = $('<div id="fancybox-title"></div>'),
 
-        nav_left = $('<a href="javascript:;" id="fancybox-left"><span class="fancy-ico" id="fancybox-left-ico"></span></a>');
-        nav_right = $('<a href="javascript:;" id="fancybox-right"><span class="fancy-ico" id="fancybox-right-ico"></span></a>');
-
-		outer.append(content, close, title, nav_left, nav_right);
+            nav_left = $('<a href="javascript:;" id="fancybox-left"><span class="fancy-ico" id="fancybox-left-ico"></span></a>'),
+            nav_right = $('<a href="javascript:;" id="fancybox-right"><span class="fancy-ico" id="fancybox-right-ico"></span></a>')
+        );
 
 		close.click($.fancybox.close);
 		loading.click($.fancybox.cancel);
